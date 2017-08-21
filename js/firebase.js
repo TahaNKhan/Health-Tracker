a99dd343ba3e21524b623ca6bd789c365604abef
unknown <taha1112@gmail.com>
2017-08-21 10:26:52 -0400

    finished basic functionality

--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	apiKey: "AIzaSyD-Q323hdpsaWtDO3aj4_oHm32Sj_R7kY8",
-	authDomain: "health-tracker-b092c.firebaseapp.com",
-	databaseURL: "https://health-tracker-b092c.firebaseio.com",
-	projectId: "health-tracker-b092c",
-	storageBucket: "health-tracker-b092c.appspot.com",
-	messagingSenderId: "16348913759"
-};
-firebase.initializeApp(config);
--- js/main.js
+++ js/main.js
@@ -33,15 +33,26 @@ $(function(){
 		el: '.app',
 		allItems:'#items',
 		total: '#totalCalories',
-
+		getFromStorage : function() {
+			var storedFood = JSON.parse(localStorage.getItem('foods'));
+			for(var i = 0; i < storedFood.length;i++){
+				foods.push(new Food({name: storedFood[i].name, calories: storedFood[i].nf_calories, brand: storedFood[i].brand}));
+			}
+		},
 		initialize: function() {
+			if(localStorage.getItem('foods') == ''){
+				localStorage.setItem('foods', JSON.stringify(foods));
+			}else{
+				this.getFromStorage();
+			}
 			this.render();
 		},
 
-
+		
 		events: {
 			'keyup #foodName' : 'lookForFoods',
-			'click .add-button' : 'addFood'
+			'click .add-button' : 'addFood',
+			'click .remove-button' : 'removeFood'
 		},
 
 		render: function(){
@@ -51,7 +62,12 @@ $(function(){
 			var foodstr = '';
 			for(var i = 0; i < foods.models.length;i++){
 				var food = foods.models[i];
-				foodstr += `\n<li>  ${food.get('name')} : ${food.get('calories')} calories. </li>`;
+				foodstr += `<tr>
+				<td>${food.get('brand')} </td>
+				<td>${food.get('name')}</td>
+				<td>${food.get('calories')} </td>
+				<td><button class="btn btn-danger remove-button" id="${i}">Remove</button></td>
+				</tr>`
 				totals += foods.models[i].get('calories');
 			}
 			$(this.allItems).html(foodstr);
@@ -60,7 +76,7 @@ $(function(){
 		},
 
 		lookForFoods : function(){
-			var food = $('#foodName').val().split(' ').join();
+			var food = $('#foodName').val();
 			if(food.length > 0){
 				var nutrXUrl = 'https://api.nutritionix.com/v1_1/search/' + food + '?results=0:10&fields=item_name,brand_name,item_id,nf_calories&appId=38e9b898&appKey=674f526031aa68144af92428d45228de'
 
@@ -85,18 +101,27 @@ $(function(){
 			}
 		},
 		addFood : function(event){
-
-			var food_id = $(event.target).attr('id');
+			var food_id = event.target.id;
 			var nutrXUrl = `https://api.nutritionix.com/v1_1/item?id=${food_id}&appId=38e9b898&appKey=674f526031aa68144af92428d45228de&fields=item_name,brand_name,item_id,nf_calories`;
 			
 			$.ajax(nutrXUrl).done((data)=>{
 
 				foods.push(new Food({name: data.item_name, calories: data.nf_calories, brand: data.brand_name}));
-
+				localStorage.setItem('foods', JSON.stringify(foods));
+				var result = $('#results');
+				result.html('');
 				this.render();
 			});
 
+			
 
+
+		},
+		removeFood : function(event){
+			console.log(foods);
+			foods.models.splice(event.target.id,1);
+			localStorage.setItem('foods', JSON.stringify(foods));
+			this.render();
 		}
 	});
 
